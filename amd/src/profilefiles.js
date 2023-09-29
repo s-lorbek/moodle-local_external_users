6d6c4dcd9808b4a54ecc01bf884c3746781acf48
Stephan Lorbek <stephan.lorbek@uni-graz.at>
2023-09-29 02:34:52 -0700

    update for user files in profile view

--- amd/src/profilefiles.js
+++ amd/src/profilefiles.js
@@ -2,8 +2,9 @@ import jQuery from 'jquery';
 
 export const append = (content) => {
     jQuery(document).ready(function() {
-        if (jQuery('.profile_tree').length > 0) {
-            jQuery('.profile_tree').append(content);
+        let profileTree = jQuery('.profile_tree');
+        if (profileTree.length > 0) {
+            profileTree.append(content);
         }
     });
 };

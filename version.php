be2fd08109936d6fcaf7351de5cb1dfe692b828c
Stephan Lorbek <stephan.lorbek@uni-graz.at>
2024-08-29 08:56:26 +0200

    taking care of enforced policies

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_external_users';
 $plugin->release = '1.9.2';
-$plugin->version = 2024082100;
+$plugin->version = 2024082600;
 $plugin->requires = 2020110900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = ['auth_external' => 2022081801];

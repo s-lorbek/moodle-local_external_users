a3ce2e7c4573d920466e13a57d357c4e61261092
Stephan Lorbek <lorbeks94@univie.ac.at>
2026-02-13 10:28:04 +0100

    Version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_external_users';
-$plugin->release = '2.0.9';
-$plugin->version = 2026021200;
+$plugin->release = '2.1.0';
+$plugin->version = 2026021300;
 $plugin->requires = 2020110900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = ['auth_external' => 2022081801];
